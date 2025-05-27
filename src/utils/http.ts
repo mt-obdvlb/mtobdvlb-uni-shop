1a62b14758a4ee2706a50586cc8debf5212cc6d8
mtobdvlb <mtobdvlb@163.com>
2025-05-27 10:11:23 +0800

    添加请求拦截器

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -4,20 +4,57 @@ const baseURL = 'https://pcapi-xiaotuxian-front-devtest.itheima.net'
 
 const httpInterceptor = {
   invoke(options: UniApp.RequestOptions) {
-    if(!options.url.startsWith('/http')){
+    if (!options.url.startsWith('/http')) {
       options.url = baseURL + options.url
     }
     options.timeout = 5000
     options.header = {
       ...options.header,
-      'source-client': 'miniapp'
+      'source-client': 'miniapp',
     }
     const memberStore = useMemberStore()
     const token = memberStore.profile?.token
-    if(token) {
+    if (token) {
       options.header.Authorization = token
     }
-  }
+  },
 }
 uni.addInterceptor('request', httpInterceptor)
 uni.addInterceptor('uploadFile', httpInterceptor)
+
+interface Data<T> {
+  code: number
+  msg: string
+  result: T
+}
+
+export const http = <T>(options: UniApp.RequestOptions) => {
+  return new Promise<Data<T>>((resolve, reject) => {
+    uni.request({
+      ...options,
+      success(res) {
+        if (res.statusCode === 200) {
+          resolve(res.data as Data<T>)
+        } else if (res.statusCode === 401) {
+          const memberStore = useMemberStore()
+          memberStore.clearProfile()
+          uni.navigateTo({ url: '/pages/login/login' })
+          reject(res)
+        } else {
+          uni.showToast({
+            icon: 'none',
+            title: (res.data as Data<T>).msg || '请求错误',
+          })
+          reject(res)
+        }
+      },
+      fail(err) {
+        uni.showToast({
+          icon: 'none',
+          title: '网络错误',
+        })
+        reject(err)
+      },
+    })
+  })
+}
