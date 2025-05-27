8762bb1b4b7bcd150b609649856837864c3edf2f
mtobdvlb <mtobdvlb@163.com>
2025-05-27 10:39:32 +0800

    feat(components): 添加全局组件 XtxSwiper
    - 新增 XtxSwiper 组件，用于轮播图展示
    - 在 pages.json 中配置全局组件路径
    - 更新类型定义，添加 GlobalComponents 接口
    - 在首页中使用 XtxSwiper 组件

---
+++ src/components/XtxSwiper.vue
@@ -0,0 +1,86 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const activeIndex = ref(0)
+</script>
+
+<template>
+  <view class="carousel">
+    <swiper :autoplay="false" :circular="true" :interval="3000">
+      <swiper-item>
+        <navigator class="navigator" hover-class="none" url="/pages/index/index">
+          <image
+            class="image"
+            mode="aspectFill"
+            src="https://pcapi-xiaotuxian-front-devtest.itheima.net/miniapp/uploads/slider_1.jpg"
+          ></image>
+        </navigator>
+      </swiper-item>
+      <swiper-item>
+        <navigator class="navigator" hover-class="none" url="/pages/index/index">
+          <image
+            class="image"
+            mode="aspectFill"
+            src="https://pcapi-xiaotuxian-front-devtest.itheima.net/miniapp/uploads/slider_2.jpg"
+          ></image>
+        </navigator>
+      </swiper-item>
+      <swiper-item>
+        <navigator class="navigator" hover-class="none" url="/pages/index/index">
+          <image
+            class="image"
+            mode="aspectFill"
+            src="https://pcapi-xiaotuxian-front-devtest.itheima.net/miniapp/uploads/slider_3.jpg"
+          ></image>
+        </navigator>
+      </swiper-item>
+    </swiper>
+    <!-- 指示点 -->
+    <view class="indicator">
+      <text
+        v-for="(item, index) in 3"
+        :key="item"
+        :class="{ active: index === activeIndex }"
+        class="dot"
+      ></text>
+    </view>
+  </view>
+</template>
+
+<style lang="scss">
+/* 轮播图 */
+.carousel {
+  height: 280rpx;
+  position: relative;
+  overflow: hidden;
+  transform: translateY(0);
+  background-color: #efefef;
+
+  .indicator {
+    position: absolute;
+    left: 0;
+    right: 0;
+    bottom: 16rpx;
+    display: flex;
+    justify-content: center;
+
+    .dot {
+      width: 30rpx;
+      height: 6rpx;
+      margin: 0 8rpx;
+      border-radius: 6rpx;
+      background-color: rgba(255, 255, 255, 0.4);
+    }
+
+    .active {
+      background-color: #fff;
+    }
+  }
+
+  .navigator,
+  .image {
+    width: 100%;
+    height: 100%;
+  }
+}
+</style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,11 +1,11 @@
-<script setup lang="ts">
-import CustomNavbar
-  from '@/pages/index/components/CustomNavbar.vue'
-
+<script lang="ts" setup>
+import CustomNavbar from '@/pages/index/components/CustomNavbar.vue'
+import XtxSwiper from '@/components/XtxSwiper.vue'
 </script>
 
 <template>
   <CustomNavbar />
+  <XtxSwiper />
   <view class="index">index</view>
 </template>
 
