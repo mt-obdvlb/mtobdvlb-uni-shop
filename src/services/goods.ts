7f8f6ff16931b4d312c45e1ec585cb35a38caa66
mtobdvlb <mtobdvlb@163.com>
2025-05-29 09:19:45 +0800

    feat(goods): 添加商品详情页面
    - 新增商品详情页面组件和相关服务
    - 定义商品信息相关类型和接口
    - 实现商品详情数据的获取和展示
    - 优化商品主图、属性详情和相似商品的展示逻辑

--- src/services/goods.ts
+++ src/services/goods.ts
@@ -1,7 +1,8 @@
 import { http } from '@/utils/http.ts'
+import type { GoodsResult } from '@/types/goods'
 
 export const getGoodsByIdAPI = (id: string) => {
-  return http({
+  return http<GoodsResult>({
     method: 'GET',
     url: `/goods`,
     data: {
